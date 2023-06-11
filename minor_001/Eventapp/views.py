cc2bb20313baf661af9ffc86ad952c44bc1f1cf4
Nataly <k1j21@yandex.ru>
2023-06-11 12:20:20 +0300

    Первый проект завершён, ура

---
+++ minor_001/Eventapp/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def main_page(request):
+    return render(request, "main.html", {"main": "active"})
+
+
+def gallery(request):
+    return render(request, "gallery.html", {"gallery": "active"})
+
+
+def order(request):
+    return render(request, "order.html", {"order": "active"})
+
+
+def scheme(request):
+    return render(request, "scheme.html", {"scheme": "active"})
+
+
+def contacts(request):
+    return render(request, "contacts.html", {"contacts": "active"})
